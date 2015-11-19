0becebd6f84b4f878186c7a6e2276827257193cc
Egor Pavlikhin <egor.pavlikhin@gmail.com>
2015-11-19 14:45:26 +0300

    Update Markdown.Editor.js

--- Markdown.Editor.js
+++ Markdown.Editor.js
@@ -1042,7 +1042,7 @@
 
             // The main dialog box.
             dialog = doc.createElement("div");
-            dialog.className = "modal hide fade";
+            dialog.className = "modal fade";
 
             modal_dialog = doc.createElement("div");
             modal_dialog.className = "modal-dialog";
